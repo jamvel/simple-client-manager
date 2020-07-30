87596ecac3dbf03aec75ce40504ead059f1a3842
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-07-31 01:49:19 +0200

    Update UserForm, UserList and UserListCard Components

--- app/components/UserList/index.jsx
+++ app/components/UserList/index.jsx
@@ -1,24 +1,31 @@
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import UserListCard from '@Components/UserListCard';
 import style from './style.module.css';
 
-const UserList = ({ users }) => (
-  <>
-    {users && Array.isArray(users) && users.length > 0 && (
-      <div className={style.listParent}>
-        {users.map((user) => (
-          <UserListCard
-            key={user.id}
-            {...user}
-          />
-        ))}
-      </div>
-    )}
-  </>
-);
+const UserList = ({ users, deleteFn }) => {
+  const router = useRouter();
+  return (
+    <>
+      {users && Array.isArray(users) && users.length > 0 && (
+        <div className={style.listParent}>
+          {users.map((user) => (
+            <UserListCard
+              key={user.id}
+              user={user}
+              editFn={() => router.push(`/user/${user.id}`)}
+              deleteFn={() => deleteFn(user.id)}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
 
 UserList.propTypes = {
   users: PropTypes.arrayOf(PropTypes.any).isRequired,
+  deleteFn: PropTypes.func.isRequired,
 };
 
 export default UserList;
