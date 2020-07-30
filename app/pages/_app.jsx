b636efdb558a71d7c06aba2a26f651d19c304a9f
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-07-30 17:50:29 +0200

    Modify config with axios baseUrl. Add global css file

--- app/pages/_app.jsx
+++ app/pages/_app.jsx
@@ -1,5 +1,10 @@
 /* eslint-disable react/prop-types */
 import Wrapper from '@Stores';
+import '@Styles/globals.css';
+import axios from 'axios';
+
+// set axios base url for relative urls calls on server
+axios.defaults.baseURL = process.env.axiosBaseUrl;
 
 function MyApp({ Component, pageProps }) {
   return (<Component {...pageProps} />);
