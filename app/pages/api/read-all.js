469f35aa6fa637d58f83ee65cfcaf3f5d7ca268b
Jamie Vella <47425412+jamvel@users.noreply.github.com>
2020-07-31 14:00:55 +0200

    Fixed eslint error

--- app/pages/api/read-all.js
+++ app/pages/api/read-all.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 /**
  * @name GET/api/read-all
  * @function
- * @param {Object} req 
- * @param {Object} res 
+ * @param {Object} req
+ * @param {Object} res
  */
 export default async (req, res) => {
   try {
